8ecbe7097d7a20046daa1bf3bf893ae3d853d612
zinabb <z.1binsumait@gmail.com>
2025-04-10 16:41:35 -0600

    Updated ProductionConfig to use SQLite

--- apps/config.py
+++ apps/config.py
@@ -1,29 +1,25 @@
-
-import os
+﻿import os
 
 class Config(object):
-
     basedir = os.path.abspath(os.path.dirname(__file__))
 
-    # Set up the App SECRET_KEY
-    # SECRET_KEY = config('SECRET_KEY'  , default='S#perS3crEt_007')
     SECRET_KEY = os.getenv('SECRET_KEY', 'S#perS3crEt_007')
 
-    # This will create a file in <app> FOLDER
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
+    # SQLite database for simplicity on Render (and locally)
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
 
-    # Assets Management
+    # Assets
     ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets')    
-    
-    SOCIAL_AUTH_GITHUB  = False
 
-    GITHUB_ID      = os.getenv('GITHUB_ID')
-    GITHUB_SECRET  = os.getenv('GITHUB_SECRET')
+    # Github social login toggle
+    SOCIAL_AUTH_GITHUB = False
+    GITHUB_ID = os.getenv('GITHUB_ID')
+    GITHUB_SECRET = os.getenv('GITHUB_SECRET')
 
-    # Enable/Disable Github Social Login    
     if GITHUB_ID and GITHUB_SECRET:
-         SOCIAL_AUTH_GITHUB  = True
+        SOCIAL_AUTH_GITHUB = True
+
 
 class ProductionConfig(Config):
     DEBUG = False
@@ -33,15 +29,8 @@ class ProductionConfig(Config):
     REMEMBER_COOKIE_HTTPONLY = True
     REMEMBER_COOKIE_DURATION = 3600
 
-    # PostgreSQL database
-    SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'.format(
-        os.getenv('DB_ENGINE'   , 'mysql'),
-        os.getenv('DB_USERNAME' , 'appseed_db_usr'),
-        os.getenv('DB_PASS'     , 'pass'),
-        os.getenv('DB_HOST'     , 'localhost'),
-        os.getenv('DB_PORT'     , 3306),
-        os.getenv('DB_NAME'     , 'appseed_db')
-    )
+    # ✅ Use the same SQLite URI as base config
+    SQLALCHEMY_DATABASE_URI = Config.SQLALCHEMY_DATABASE_URI
 
 
 class DebugConfig(Config):
@@ -52,4 +41,6 @@ class DebugConfig(Config):
 config_dict = {
     'Production': ProductionConfig,
     'Debug'     : DebugConfig
+}
+
 }
